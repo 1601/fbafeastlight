1521147801096dd0a93ad9ff722ed6c1aab6ac37
Gelo Ilagan <gelo@ussi.com.ph>
2020-03-07 20:44:03 +0800

    first commit

---
+++ src/components/main.js
@@ -0,0 +1,96 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { makeStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
+import Button from "@material-ui/core/Button"
+import Paper from "@material-ui/core/Paper"
+import Hidden from "@material-ui/core/Hidden"
+import { Room, AccessTime } from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(0, 3),
+  },
+  paper: {
+    maxWidth: 700,
+    margin: `${theme.spacing(1)}px auto`,
+    padding: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: 400,
+    },
+  },
+}));
+
+const events = [
+  {
+    date: "01 July",
+    title: "Coffee Date",
+    time: "9:00 AM -11:00 AM",
+    location: "PICC Banquet Hall"
+  },
+  {
+    date: "20 July",
+    title: "1st Forum",
+    time: "10:00 AM -1:00 PM",
+    location: "DReg Room"
+  },
+  {
+    date: "02 August",
+    title: "The Light Project",
+    time: "10:00 AM - 01:00 PM",
+    location: "Room"
+  }]
+export default function Main(props) {
+  const classes = useStyles()
+  const { posts, title } = props
+
+  return (
+    <Grid item xs={12} md={8}>
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+      {events.map(event => (
+
+      <Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Button color="secondary">{event.date}</Button>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">
+              {event.title}
+            </Typography>
+          </Grid>
+          <Hidden smDown>
+            <Grid item>
+              <AccessTime></AccessTime>
+            </Grid>
+          
+            <Grid item>
+              <Button color="primary">{event.time}</Button>
+            </Grid>
+          </Hidden>
+          <Hidden smDown>
+            <Grid item>
+              <Room></Room>
+            </Grid>
+          </Hidden>
+          <Grid item>
+            <Button color="primary">{event.location}</Button>
+          </Grid>
+        </Grid>
+      </Paper>
+      ))}
+    </Grid>
+  )
+}
+
+Main.propTypes = {
+  events: PropTypes.array,
+  title: PropTypes.string,
+}
---
+++ src/pages/index.js
@@ -0,0 +1,145 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import Grid from "@material-ui/core/Grid"
+import Container from "@material-ui/core/Container"
+import GitHubIcon from "@material-ui/icons/GitHub"
+import FacebookIcon from "@material-ui/icons/Facebook"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import Header from "../components/header"
+import MainFeaturedPost from "../components/mainFeaturedPost"
+import FeaturedPost from "../components/featuredPost"
+import Main from "../components/main"
+import Sidebar from "../components/sideBar"
+import Footer from "../components/footer"
+import Typography from "@material-ui/core/Typography"
+
+const useStyles = makeStyles(theme => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}))
+
+const sections = [
+  { title: "Home", url: "#" },
+  { title: "About Us", url: "#" },
+  { title: "Feast Live Schedule & Location", url: "#" },
+  { title: "Feast Talks", url: "#" },
+  { title: "Feast Light Locations", url: "#" },
+  { title: "Contact Us", url: "#" },
+]
+
+const mainFeaturedPost = {
+  title: "Make Disciples",
+  description:
+    "BRING PEOPLE. CLOSER TO JESUS.",
+  image: "https://www.fbafeastlight.com/static/creative-4b1a8f84c8e861834bca4f04b5dcbf9e.png",
+  imgText: "compilation of feast light talks",
+  linkText: "Join for free.",
+}
+
+const featuredPosts = [
+  {
+    title: "Featured post",
+    date: "Nov 12",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+  {
+    title: "Post title",
+    date: "Nov 11",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+]
+
+const posts = [featuredPosts[0]]
+
+const sidebar = {
+  title: "About",
+  description:
+    "​The Feast Light is a small gathering of friends who want to receive spiritual nourishment from the series of talks recorded from a live audience at The Feast.",
+  archives: [
+    { title: "March 2020", url: "#" },
+    { title: "February 2020", url: "#" },
+    { title: "January 2020", url: "#" },
+    { title: "November 2019", url: "#" },
+    { title: "October 2019", url: "#" },
+    { title: "September 2019", url: "#" },
+    { title: "August 2019", url: "#" },
+    { title: "July 2019", url: "#" },
+    { title: "June 2019", url: "#" },
+    { title: "May 2019", url: "#" },
+    { title: "April 2019", url: "#" },
+  ],
+  social: [
+    { name: "Twitter", icon: TwitterIcon },
+    { name: "Facebook", icon: FacebookIcon },
+  ],
+}
+
+const bibleVerses = {
+  body: `“‘Come, follow me,’ Jesus said, ‘and I will send you out to fish for people.’ At once they left their nets and followed him.”`,
+  verse: 'Matthew 4:19-20',
+  subtitle: 'God often uses the stories of fisherman in His Word. He wants us to literally “fish for men.” When He asks them to leave their nets, He’s talking about leaving our security, our comfort, and all that we know and follow HIM! For aren’t men more important than “fish?”'
+}
+
+export default function Blog() {
+  const classes = useStyles()
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="the FEAST Light Bay Area" sections={sections} />
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          {/* <Grid container spacing={4}>
+            {featuredPosts.map(post => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid> */}
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography align="center" variant="body1" color="inherit" paragraph>
+                {bibleVerses.body}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography align="center" variant="caption"  color="error" display="block" paragraph>
+                <i>{bibleVerses.verse}</i>
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography align="center" variant="body2" color="inherit" paragraph>
+                {bibleVerses.subtitle}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Main title="Upcoming Events" />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
+      </Container>
+      <Footer
+        title="Feast Light Bay Area"
+        description="Make Disciples. Bring People. Closer to Jesus."
+      />
+    </React.Fragment>
+  )
+}
