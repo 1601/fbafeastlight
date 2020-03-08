d5236c49b0c3e392d0e3934df2ca35e0cb978878
Babyboy <dmcilagan@gmail.com>
2020-03-08 21:33:35 +0800

    updated sign in and move signOutButton
    also removed unnecessary code files

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import { StyledFirebaseAuth } from 'react-firebaseui'
+import React from "react"
+import { StyledFirebaseAuth } from "react-firebaseui"
+import { CssBaseline, Container, Grid, Typography } from "@material-ui/core"
+import Header from "../components/header"
+import Footer from "../components/footer"
+import { getUiConfig } from "../firebase"
+import { withFirebase } from "../components/FirebaseContext"
 
-import { getUiConfig } from '../firebase'
-import { withFirebase } from '../components/FirebaseContext'
+const signInText = {
+  title: "Hi, please login to view & download exclusive content.",
+  body: "Having trouble? Ask for help on our chat support.",
+}
 
 const SignIn = ({ firebase }) => (
-  <div>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h4>Hey, please login to continue</h4>
-      <h5>This is just firebase login test</h5>
-    </div>
-    <StyledFirebaseAuth
-      uiConfig={getUiConfig(firebase)}
-      firebaseAuth={firebase.auth()}
+  <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <Header title="the FEAST Light Bay Area" />
+      <main>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <Grid item>
+            <Typography align="center" variant="h6" color="inherit" paragraph>
+              {signInText.title}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              align="center"
+              variant="p"
+              color="error"
+              display="block"
+              paragraph
+            >
+              <i>{signInText.body}</i>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <StyledFirebaseAuth
+              uiConfig={getUiConfig(firebase)}
+              firebaseAuth={firebase.auth()}
+            />
+          </Grid>
+        </Grid>
+      </main>
+    </Container>
+    <Footer
+      title="Feast Light Bay Area"
+      description="Make Disciples. Bring People. Closer to Jesus."
     />
-  </div>
+  </React.Fragment>
 )
 
 export default withFirebase(SignIn)
